bc6ee62e590101e7b369bc0359d30e18fdf010ac
Frandel <lo-masbacano04@outlook.com>
2025-12-22 16:45:58 -0400

    Desing ready

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,18 +5,27 @@ export default function Layout({ children }: PropsWithChildren) {
 
     return (
         // {children}
-        <div className="flex">
-            <div className="w-[60%] h-screen bg-[#212121] p-5">
+        <div className="w-screen h-screen flex bg-[#181818] flex-col items-center  lg:flex-row justify-center transition-all duration-600">
+            <div className="lg:w-[60%] lg:h-screen  lg:p-5  lg:bg-[#181818] text-center">
 
-                <div className="h-full bg-cover bg-center flex flex-col  p-16 pt-16 rounded-lg"
-                    style={{ backgroundImage: "url('/fondo.jpg')" }}>
+                <div className="lg:h-full
+                                flex flex-col  
+                                lg:p-16 
+                                p-5
+                                rounded-lg 
+                                bg-none
+                                lg:bg-[url('/fondo.jpg')]
+                                lg:bg-cover
+                                lg:bg-center">
 
-                    <div className="w-50">
+                    <div className="w-50 hidden lg:block transition-all duration-600">
                         <img src="logo2.png" alt="SynkNote logo" />
                     </div>
-                    <div className="w-160 ">
-                        <h1 className="  inline-block text-white  text-5xl font-bold tracking-wide mb-32 transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(233,86,59,1)]">SynkNote</h1>
-                        <p className="text-2xl text-white font-thin ">This is an encrypted messaging application in which messages are automatically deleted at regular intervals, ensuring that no trace of the conversations remains.</p>
+                    <div className="w-auto lg:text-start">
+                        <h1 className=" inline-block text-white  text-3xl lg:text-5xl font-bold tracking-wide lg:mb-32 transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(233,86,59,1)]">SynkNote</h1>
+                        <p className=" 2xl:text-2xl  text-white font-thin hidden lg:inline-block">This is an encrypted messaging application in which messages are automatically deleted at regular intervals, ensuring that no trace of the conversations remains.</p>
+
+
                     </div>
                 </div>
             </div>
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,31 +1,112 @@
 export const RegisterForm = () => {
 
     return (
-        <div className="bg-[#212121] w-[40%] flex flex-col justify-center items-center">
+        <div className=" lg:w-[40%] 
+                         lg:h-screen 
+                         bg-[#212121] 
+                         flex 
+                         flex-col 
+                         justify-center 
+                         items-center 
+                         rounded-[10px] 
+                         lg:rounded-none">
 
+            <div className="w-70 
+                            sm:w-90 
+                            lg:w-95 
+                            2xl:w-105  
+                            p-2  
+                            transition-all 
+                            duration-600">
 
+                <h1 className=" text-white
+                                 text-[20px] 
+                                 lg:text-[38px] 
+                                 font-bold
+                                 text-center 
+                                 transition-all
+                                 duration-600">Create an account
+                </h1>
 
-            <div className="w-100">
-                <h1 className=" text-white  text-[38px] font-bold text-center ">Create an account</h1>
-                <h2 className="text-white/40 text-center mb-15">It`s quick and easy.</h2>
-                <form action="#" className="flex flex-col gap-5">
+                <h2 className="text-white/40 
+                                text-[13px] 
+                                lg:text-[18px] 
+                                text-center 
+                                mb-15 
+                                transition-all 
+                                duration-600">It's quick and easy.
+                </h2>
+
+                <form action="#" className="flex 
+                                            flex-col 
+                                            gap-5">
                     <label htmlFor="name" className="sr-only ">Name</label>
-                    <input type="text" name="name" id="" placeholder="Name" required className="w-auto p-2.5 border-b border-white/30 outline-none focus:border-white/80 focus:bg-white/30 rounded-[10px] placeholder:text-white/50 text-white transition-all duration-500" />
-
-                    <label htmlFor="lastName" className="sr-only ">Last Name</label>
-                    <input type="text" name="lastName" id="" placeholder="Last Name" required className="w-auto p-2.5 border-b border-white/30 outline-none focus:border-white/80 focus:bg-white/30 rounded-[10px] placeholder:text-white/50 text-white transition-all duration-500" />
+                    <input type="text" name="name" id="" placeholder="Name" required className="w-auto 
+                                                                                                p-2.5 
+                                                                                                border-b
+                                                                                                 border-white/30 
+                                                                                                 outline-none
+                                                                                                  focus:border-white/80
+                                                                                                  focus:bg-white/30 
+                                                                                                 rounded-[10px]
+                                                                                                  placeholder:text-white/50
+                                                                                                  text-white
+                                                                                                  transition-all
+                                                                                                  duration-500 
+                                                                                                 text-[12px] 
+                                                                                                 lg:text-[16px]" />
 
                     <label htmlFor="email" className="sr-only ">Email</label>
-                    <input type="email" name="email" id="" placeholder="Email" required className="w-auto p-2.5 border-b border-white/30 outline-none focus:border-white/80 focus:bg-white/30 rounded-[10px] placeholder:text-white/50 text-white transition-all duration-500" />
+                    <input type="email" name="email" id="" placeholder="Email" required className="w-auto 
+                                                                                                    p-2.5 
+                                                                                                    border-b
+                                                                                                     border-white/30 
+                                                                                                    outline-none
+                                                                                                     focus:border-white/80
+                                                                                                     focus:bg-white/30 
+                                                                                                    rounded-[10px]
+                                                                                                     placeholder:text-white/50
+                                                                                                     text-white
+                                                                                                     transition-all 
+                                                                                                    duration-500 
+                                                                                                    text-[12px] 
+                                                                                                    lg:text-[16px]" />
 
                     <label htmlFor="password" className="sr-only">Password</label>
-                    <input type="password" name="password" id="" placeholder="Password" required className="w-auto p-2.5 border-b border-white/30 outline-none focus:border-white/80 focus:bg-white/30 rounded-[10px] placeholder:text-white/50 text-white transition-all duration-500" />
+                    <input type="password" name="password" id="" placeholder="Password" required className="w-auto 
+                                                                                                            p-2.5 
+                                                                                                            border-b
+                                                                                                             border-white/30 
+                                                                                                            outline-none
+                                                                                                             focus:border-white/80
+                                                                                                             focus:bg-white/30 
+                                                                                                            rounded-[10px]
+                                                                                                             placeholder:text-white/50
+                                                                                                             text-white
+                                                                                                             transition-all 
+                                                                                                            duration-500 
+                                                                                                            text-[12px] 
+                                                                                                            lg:text-[16px]" />
 
                     <label htmlFor="password" className="sr-only">Confirm Password</label>
-                    <input type="password" name="confirmPassword" id="" placeholder="Confirm Password" required className="w-auto p-2.5 border-b border-white/30 outline-none focus:border-white/80 focus:bg-white/30 rounded-[10px] placeholder:text-white/50 text-white transition-all duration-500" />
+                    <input type="password" name="confirmPassword" id="" placeholder="Confirm Password" required className="w-auto 
+                                                                                                                             p-2.5 
+                                                                                                                             border-b
+                                                                                                                             border-white/30 
+                                                                                                                             outline-none
+                                                                                                                             focus:border-white/80
+                                                                                                                             focus:bg-white/30 
+                                                                                                                             rounded-[10px]
+                                                                                                                             placeholder:text-white/50
+                                                                                                                             text-white 
+                                                                                                                             transition-all 
+                                                                                                                             duration-500 
+                                                                                                                             text-[12px] 
+                                                                                                                             lg:text-[16px]"
+                    />
 
-                    <button type="submit" className="bg-[#db4437] w-auto p-2.5 mt-5 text-[16px] text-white/90 font-medium rounded-[10px] cursor-pointer">Register</button>
-                    <a href="#" className="mx-auto text-[15px] text-white">Do you already have an account?</a>
+                    <button type="submit" className="bg-[#db4437] w-auto p-2.5 mt-5 text-[12px] lg:text-[16px] text-white/90 font-medium rounded-[10px] cursor-pointer">Register</button>
+                    <a href="#" className="mx-auto text-[10px] lg:text-[14px] text-white">Do you already have an account?</a>
                 </form>
             </div>
 
